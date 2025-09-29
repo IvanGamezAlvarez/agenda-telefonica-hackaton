78b755ea7da2156cf2d8be2a7a23f43dc6c72a1a
Ivan <thecubeg@gmail.com>
2025-09-29 15:46:15 -0600

    feat: contacto class

---
+++ yellow-section/src/main/java/com/yellowsection/Contacto.java
@@ -0,0 +1,61 @@
+package com.yellowsection;
+
+import java.util.Objects;
+
+public class Contacto {
+	private String nombre;
+    private String apellido;
+    private String telefono;
+
+    public Contacto(String nombre, String apellido, String telefono) {
+        if (nombre == null || nombre.trim().isEmpty() || apellido == null || apellido.trim().isEmpty()) {
+            throw new IllegalArgumentException("El nombre y el apellido no pueden estar vacíos.");
+        }
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contacto contacto = (Contacto) o;
+        return Objects.equals(nombre.toLowerCase(), contacto.nombre.toLowerCase());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre.toLowerCase());
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto -> Nombre: " + nombre + " " + apellido + " | Teléfono: " + telefono;
+    }
+
+}
