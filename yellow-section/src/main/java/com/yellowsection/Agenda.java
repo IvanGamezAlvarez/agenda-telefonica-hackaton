9655750e8a9d4dbc394fa4608a33301abf2b488e
Ivan <thecubeg@gmail.com>
2025-09-29 12:38:43 -0600

    feat: maven init

---
+++ yellow-section/src/main/java/com/yellowsection/Agenda.java
@@ -0,0 +1,20 @@
+package com.yellowsection;
+
+public class Agenda {
+	
+	Contacto[] listaContactos;
+	
+	
+	public Agenda(int arraySize) {
+		this.listaContactos = new Contacto[arraySize];
+	}
+	public Agenda() {
+		this(10);
+	}
+}
+
+
+
+class Contacto{
+	
+}
